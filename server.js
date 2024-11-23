6b8278ed00081fbd233ca0c0122f8d0ae411a23c
BasavarajuVB <bellamvadde0393@gmail.com>
2024-11-23 17:16:05 +0530

    updated code

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// // Database connection
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-connectDB();
+// Database connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.use('/api/notes', require('./routes/notes'));
